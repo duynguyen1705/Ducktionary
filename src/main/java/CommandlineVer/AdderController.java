75a330595f8e4d7407d0ac928d7f1698d691387b
pknguyen2704 <pknguyen2704@gmail.com>
2023-11-18 03:46:51 +0700

    Update AdderController by Andrew Phung

--- src/main/java/CommandlineVer/AdderController.java
+++ src/main/java/CommandlineVer/AdderController.java
@@ -1,53 +1,121 @@
 package CommandlineVer;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AdderController implements Initializable {
+    private DictionaryManagement dictionaryManagement = new DictionaryManagement();
+
+    // cai nay neu su dung txt
+    private final String path = "src/main/resources/dictionaries.txt";
+    @FXML
+    private TextField WordTarget;
 
     @FXML
-    TextField wordName;
+    private TextField WordExplanation;
 
     @FXML
-    TextField wordExplaination;
+    private ChoiceBox<String> WordType;
 
     @FXML
-    ChoiceBox<String> wordType;
-
-    @FXML
-    TextField wordExample;
+    private TextField WordExample;
 
     @FXML
     Word newWord;
 
-    public void createWord() {
-        System.out.println("Button clicked! \n");
-        try {
-            String wordNameText = wordName.getText();
-            String wordTypeText = wordType.getValue();
-            String wordExplainationText = wordExplaination.getText();
-            String wordExampleText = wordExample.getText();
-
-            if (wordNameText == "" || wordExplainationText == "" || wordExampleText == "") {
-                throw new RuntimeException("Missing components");
-            }
-
-
-
-
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
-    }
-
+    @FXML
+    private Button AdderSubmitBtn1;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        String[] allWordType = {"Danh từ", "Động từ", "Tính từ", "Trạng từ", "Trợ động từ", "Khác"};
-        wordType.getItems().addAll(allWordType);
+        dictionaryManagement.insertFromFile(Dictionary.dictionary, path);
+        if(WordExplanation.getText().isEmpty() || WordTarget.getText().isEmpty()) {
+            AdderSubmitBtn1.setDisable(true);
+        }
+
+        WordTarget.setOnKeyTyped(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (WordExplanation.getText().isEmpty() || WordTarget.getText().isEmpty()) {
+                   AdderSubmitBtn1.setDisable(true);
+                } else
+                    AdderSubmitBtn1.setDisable(false);
+            }
+        });
+
+        WordExplanation.setOnKeyTyped(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (WordExplanation.getText().isEmpty() || WordTarget.getText().isEmpty()) {
+                    AdderSubmitBtn1.setDisable(true);
+                } else
+                    AdderSubmitBtn1.setDisable(false);
+            }
+        });
     }
+    public void createWord(ActionEvent e) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Add word");
+        alert.setContentText("Do you want to add this word");
+
+        Optional<ButtonType> option = alert.showAndWait();
+        String wordTarget = WordTarget.getText().trim();
+        String wordExplanation = WordExplanation.getText().trim();
+        String wordType = WordType.getValue();
+        String wordExample = WordExample.getText().trim();
+
+
+        if (option.get() == ButtonType.OK) {
+             Word newWord = new Word();
+             newWord.setWordTarget(wordTarget);
+             newWord.setWordExplain(wordExplanation);
+             newWord.setWordType(wordType);
+             if (Dictionary.dictionary.contains(newWord)) {
+                 int indexOfWord = dictionaryManagement.searchWord(Dictionary.dictionary, wordTarget);
+                 Alert selectionAlert = new Alert(Alert.AlertType.INFORMATION);
+                 selectionAlert.setTitle("This word is exist");
+                 selectionAlert.setContentText("Sorry, this word is contained in Dictionary \n You only can update content for this word");
+                 selectionAlert.getButtonTypes().clear();
+                 
+                 ButtonType replace = new ButtonType("Replace");
+                 ButtonType update = new ButtonType("Update");
+
+                 selectionAlert.getButtonTypes().addAll(replace, update, ButtonType.CANCEL);
+                 Optional<ButtonType> select = selectionAlert.showAndWait();
+                 Alert alertSuccess = new Alert(Alert.AlertType.INFORMATION);
+
+                 if(select.get() == replace) {
+                     Dictionary.dictionary.get(indexOfWord).setWordExplain(wordExplanation);
+                     dictionaryManagement.dictionaryExportToFile(Dictionary.dictionary, path);
+                     alertSuccess.setTitle("Successfull");
+                 }
+
+                 if(select.get() == update) {
+                     String oldExplain = Dictionary.dictionary.get(indexOfWord).getWordExplain();
+                     Dictionary.dictionary.get(indexOfWord).setWordExplain(oldExplain + "\n=" + wordExplanation);
+                     dictionaryManagement.dictionaryExportToFile(Dictionary.dictionary, path);
+                     alertSuccess.setTitle("Successfull");
+                 }
+                 if(select.get() == ButtonType.CANCEL) {
+                     Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+                     newAlert.setTitle("ALERT");
+                     newAlert.setTitle("Oh no, your word is not updated");
+                 }
+             } else {
+                 Dictionary.dictionary.add(newWord);
+                 dictionaryManagement.addWord(Dictionary.dictionary, path);
+             }
+        }
+
+
+    }
+
+
 }
--- src/main/java/CommandlineVer/WindowTest.java
+++ src/main/java/CommandlineVer/WindowTest.java
@@ -7,11 +7,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class WindowTest extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/DictionaryWindow.fxml"));
+        Parent root = FXMLLoader.load((getClass().getResource("/DictionaryWindow.fxml")));
         Scene adderScene = new Scene(root, 600, 400);
         stage.setScene(adderScene);
         stage.show();
